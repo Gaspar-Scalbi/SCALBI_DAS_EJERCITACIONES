75ce86be8341a69b611dd0094069b6b60fcb4ce6
Gaspar SCalbi <gasparscalbi@gmail.com>
2025-09-24 11:11:50 -0300

    Inicializado el FORM general para mostrar las tablas. Restante: Codear los formularios y crear los ingresos de datos

--- TP_2-CRUD_SQL_ADO.NET/PRÁCTICA_ADO/Form1.Designer.cs
+++ TP_2-CRUD_SQL_ADO.NET/PRÁCTICA_ADO/Form1.Designer.cs
@@ -30,19 +30,33 @@
         {
             btnSalir = new Button();
             label1 = new Label();
-            btnAgregar = new Button();
-            btnEliminar = new Button();
-            btnModificar = new Button();
+            btnAgregarAvion = new Button();
+            btnEliminarAvion = new Button();
+            btnModificarAvion = new Button();
             dgvTabla = new DataGridView();
+            btnAgregarPasajero = new Button();
+            btnEliminarPasajero = new Button();
+            btnModificarPasajero = new Button();
+            dataGridView1 = new DataGridView();
+            btnAgregarPasaje = new Button();
+            btnEliminarPasaje = new Button();
+            btnModificarPasaje = new Button();
+            dataGridView2 = new DataGridView();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvTabla).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // btnSalir
             // 
-            btnSalir.Location = new Point(406, 332);
+            btnSalir.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point, 1, true);
+            btnSalir.Location = new Point(530, 412);
             btnSalir.Margin = new Padding(2);
             btnSalir.Name = "btnSalir";
-            btnSalir.Size = new Size(78, 28);
+            btnSalir.Size = new Size(265, 44);
             btnSalir.TabIndex = 11;
             btnSalir.Text = "Salir";
             btnSalir.UseVisualStyleBackColor = true;
@@ -51,42 +65,42 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            label1.Location = new Point(300, 67);
+            label1.Location = new Point(572, 9);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
             label1.Size = new Size(184, 21);
             label1.TabIndex = 10;
             label1.Text = "GESTIÓNAR TABLA SQL";
             // 
-            // btnAgregar
+            // btnAgregarAvion
             // 
-            btnAgregar.Location = new Point(287, 301);
-            btnAgregar.Margin = new Padding(2);
-            btnAgregar.Name = "btnAgregar";
-            btnAgregar.Size = new Size(78, 27);
-            btnAgregar.TabIndex = 9;
-            btnAgregar.Text = "Agregar";
-            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregarAvion.Location = new Point(85, 317);
+            btnAgregarAvion.Margin = new Padding(2);
+            btnAgregarAvion.Name = "btnAgregarAvion";
+            btnAgregarAvion.Size = new Size(78, 27);
+            btnAgregarAvion.TabIndex = 9;
+            btnAgregarAvion.Text = "Agregar";
+            btnAgregarAvion.UseVisualStyleBackColor = true;
             // 
-            // btnEliminar
+            // btnEliminarAvion
             // 
-            btnEliminar.Location = new Point(406, 301);
-            btnEliminar.Margin = new Padding(2);
-            btnEliminar.Name = "btnEliminar";
-            btnEliminar.Size = new Size(78, 27);
-            btnEliminar.TabIndex = 8;
-            btnEliminar.Text = "Eliminar";
-            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminarAvion.Location = new Point(322, 318);
+            btnEliminarAvion.Margin = new Padding(2);
+            btnEliminarAvion.Name = "btnEliminarAvion";
+            btnEliminarAvion.Size = new Size(78, 27);
+            btnEliminarAvion.TabIndex = 8;
+            btnEliminarAvion.Text = "Eliminar";
+            btnEliminarAvion.UseVisualStyleBackColor = true;
             // 
-            // btnModificar
+            // btnModificarAvion
             // 
-            btnModificar.Location = new Point(287, 332);
-            btnModificar.Margin = new Padding(2);
-            btnModificar.Name = "btnModificar";
-            btnModificar.Size = new Size(78, 28);
-            btnModificar.TabIndex = 7;
-            btnModificar.Text = "Modificar";
-            btnModificar.UseVisualStyleBackColor = true;
+            btnModificarAvion.Location = new Point(206, 316);
+            btnModificarAvion.Margin = new Padding(2);
+            btnModificarAvion.Name = "btnModificarAvion";
+            btnModificarAvion.Size = new Size(78, 28);
+            btnModificarAvion.TabIndex = 7;
+            btnModificarAvion.Text = "Modificar";
+            btnModificarAvion.UseVisualStyleBackColor = true;
             // 
             // dgvTabla
             // 
@@ -98,23 +112,155 @@
             dgvTabla.Name = "dgvTabla";
             dgvTabla.RowHeadersWidth = 62;
             dgvTabla.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvTabla.Size = new Size(576, 169);
+            dgvTabla.Size = new Size(265, 169);
             dgvTabla.TabIndex = 6;
+            // 
+            // btnAgregarPasajero
+            // 
+            btnAgregarPasajero.Location = new Point(504, 317);
+            btnAgregarPasajero.Margin = new Padding(2);
+            btnAgregarPasajero.Name = "btnAgregarPasajero";
+            btnAgregarPasajero.Size = new Size(78, 27);
+            btnAgregarPasajero.TabIndex = 15;
+            btnAgregarPasajero.Text = "Agregar";
+            btnAgregarPasajero.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarPasajero
+            // 
+            btnEliminarPasajero.Location = new Point(750, 319);
+            btnEliminarPasajero.Margin = new Padding(2);
+            btnEliminarPasajero.Name = "btnEliminarPasajero";
+            btnEliminarPasajero.Size = new Size(78, 27);
+            btnEliminarPasajero.TabIndex = 14;
+            btnEliminarPasajero.Text = "Eliminar";
+            btnEliminarPasajero.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarPasajero
+            // 
+            btnModificarPasajero.Location = new Point(627, 318);
+            btnModificarPasajero.Margin = new Padding(2);
+            btnModificarPasajero.Name = "btnModificarPasajero";
+            btnModificarPasajero.Size = new Size(78, 28);
+            btnModificarPasajero.TabIndex = 13;
+            btnModificarPasajero.Text = "Modificar";
+            btnModificarPasajero.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(530, 114);
+            dataGridView1.Margin = new Padding(2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(265, 169);
+            dataGridView1.TabIndex = 12;
+            // 
+            // btnAgregarPasaje
+            // 
+            btnAgregarPasaje.Location = new Point(888, 317);
+            btnAgregarPasaje.Margin = new Padding(2);
+            btnAgregarPasaje.Name = "btnAgregarPasaje";
+            btnAgregarPasaje.Size = new Size(78, 27);
+            btnAgregarPasaje.TabIndex = 19;
+            btnAgregarPasaje.Text = "Agregar";
+            btnAgregarPasaje.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarPasaje
+            // 
+            btnEliminarPasaje.Location = new Point(1156, 317);
+            btnEliminarPasaje.Margin = new Padding(2);
+            btnEliminarPasaje.Name = "btnEliminarPasaje";
+            btnEliminarPasaje.Size = new Size(78, 27);
+            btnEliminarPasaje.TabIndex = 18;
+            btnEliminarPasaje.Text = "Eliminar";
+            btnEliminarPasaje.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarPasaje
+            // 
+            btnModificarPasaje.Location = new Point(1019, 316);
+            btnModificarPasaje.Margin = new Padding(2);
+            btnModificarPasaje.Name = "btnModificarPasaje";
+            btnModificarPasaje.Size = new Size(78, 28);
+            btnModificarPasaje.TabIndex = 17;
+            btnModificarPasaje.Text = "Modificar";
+            btnModificarPasaje.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView2.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(916, 114);
+            dataGridView2.Margin = new Padding(2);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 62;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.Size = new Size(265, 169);
+            dataGridView2.TabIndex = 16;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label2.Location = new Point(123, 81);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(224, 21);
+            label2.TabIndex = 20;
+            label2.Text = "GESTIÓNAR TABLA AVIONES";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label3.Location = new Point(542, 81);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(243, 21);
+            label3.TabIndex = 21;
+            label3.Text = "GESTIÓNAR TABLA PASAJEROS";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label4.Location = new Point(941, 81);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(221, 21);
+            label4.TabIndex = 22;
+            label4.Text = "GESTIÓNAR TABLA PASAJES";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1304, 529);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(btnAgregarPasaje);
+            Controls.Add(btnEliminarPasaje);
+            Controls.Add(btnModificarPasaje);
+            Controls.Add(dataGridView2);
+            Controls.Add(btnAgregarPasajero);
+            Controls.Add(btnEliminarPasajero);
+            Controls.Add(btnModificarPasajero);
+            Controls.Add(dataGridView1);
             Controls.Add(btnSalir);
             Controls.Add(label1);
-            Controls.Add(btnAgregar);
-            Controls.Add(btnEliminar);
-            Controls.Add(btnModificar);
+            Controls.Add(btnAgregarAvion);
+            Controls.Add(btnEliminarAvion);
+            Controls.Add(btnModificarAvion);
             Controls.Add(dgvTabla);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dgvTabla).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -123,9 +269,20 @@
 
         private Button btnSalir;
         private Label label1;
-        private Button btnAgregar;
-        private Button btnEliminar;
-        private Button btnModificar;
+        private Button btnAgregarAvion;
+        private Button btnEliminarAvion;
+        private Button btnModificarAvion;
         private DataGridView dgvTabla;
+        private Button btnAgregarPasajero;
+        private Button btnEliminarPasajero;
+        private Button btnModificarPasajero;
+        private DataGridView dataGridView1;
+        private Button btnAgregarPasaje;
+        private Button btnEliminarPasaje;
+        private Button btnModificarPasaje;
+        private DataGridView dataGridView2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
