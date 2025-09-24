4b23c6150aabd269d8fdb838cdf04100bc19b506
Gaspar SCalbi <gasparscalbi@gmail.com>
2025-09-24 10:50:53 -0300

    Práctica ADO - Multiples tablas

---
+++ TP_2-CRUD_SQL_ADO.NET/PRÁCTICA_ADO/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ado_multiplestablas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSalir = new Button();
+            label1 = new Label();
+            btnAgregar = new Button();
+            btnEliminar = new Button();
+            btnModificar = new Button();
+            dgvTabla = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvTabla).BeginInit();
+            SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(406, 332);
+            btnSalir.Margin = new Padding(2);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(78, 28);
+            btnSalir.TabIndex = 11;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.Location = new Point(300, 67);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(184, 21);
+            label1.TabIndex = 10;
+            label1.Text = "GESTIÓNAR TABLA SQL";
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(287, 301);
+            btnAgregar.Margin = new Padding(2);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(78, 27);
+            btnAgregar.TabIndex = 9;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(406, 301);
+            btnEliminar.Margin = new Padding(2);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(78, 27);
+            btnEliminar.TabIndex = 8;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            btnModificar.Location = new Point(287, 332);
+            btnModificar.Margin = new Padding(2);
+            btnModificar.Name = "btnModificar";
+            btnModificar.Size = new Size(78, 28);
+            btnModificar.TabIndex = 7;
+            btnModificar.Text = "Modificar";
+            btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // dgvTabla
+            // 
+            dgvTabla.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvTabla.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvTabla.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvTabla.Location = new Point(100, 114);
+            dgvTabla.Margin = new Padding(2);
+            dgvTabla.Name = "dgvTabla";
+            dgvTabla.RowHeadersWidth = 62;
+            dgvTabla.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvTabla.Size = new Size(576, 169);
+            dgvTabla.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalir);
+            Controls.Add(label1);
+            Controls.Add(btnAgregar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnModificar);
+            Controls.Add(dgvTabla);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvTabla).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSalir;
+        private Label label1;
+        private Button btnAgregar;
+        private Button btnEliminar;
+        private Button btnModificar;
+        private DataGridView dgvTabla;
+    }
+}
---
+++ TP_2-CRUD_SQL_ADO.NET/PRÁCTICA_ADO/RepositorioPasajes.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ado_multiplestablas
+{
+    public class RepositorioPasajes
+    {
+        private string cadenaConexion = "Data Source=RCAL1306P09-111;Initial Catalog=PasajesAviones;Integrated Security=True;Persist Security Info=False;Pooling=False;Encrypt=False";
+        private List<Avion> listaAviones;
+        private List<Pasaje> listaPasajes;
+        private List<Pasajero> listaPasajeros;
+
+        public RepositorioPasajes()
+        {
+            listaAviones = new List<Avion>();
+            listaPasajeros = new List<Pasajero>();
+            listaPasajes = new List<Pasaje>();
+        }
+
+        //REPOSITORO 
+
+        public void AgregarAvion(Avion avion)
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Avion (Matricula, Modelo, Capacidad) VALUES (@Matricula, @Modelo, @Capacidad)";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Matricula", avion.Matricula);
+                    cmd.Parameters.AddWithValue("@Modelo", avion.Modelo);
+                    cmd.Parameters.AddWithValue("@Capacidad", avion.Capacidad);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void ModificarAvion(Avion avion)
+        {
+            string querry = "UPDATE Avion SET Matricula=@matricula, Modelo=@modelo, Capacidad=@capacidad " +
+                "WHERE IdAvion=@idAvion";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand comando = new SqlCommand(querry, conexion);
+                comando.Parameters.AddWithValue("@matricula", avion.Matricula);
+                comando.Parameters.AddWithValue("@modelo", avion.Modelo);
+                comando.Parameters.AddWithValue("@capacidad", avion.Capacidad);
+                comando.Parameters.AddWithValue("@idAvion", avion.IdAvion);
+
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error en la base de datos" + ex.Message);
+                }
+            }
+        }
+
+        public void EliminarAvion(int idAvion)
+        {
+            string querry = "DELETE From Avion WHERE IdAvion=@idAvion";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand comando = new SqlCommand(querry, conexion);
+                comando.Parameters.AddWithValue("@idAvion", idAvion);
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error en la base de datos" + ex.Message);
+                }
+            }
+        }
+        public IReadOnlyCollection<Avion> ListarAviones()
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                conn.Open();
+                string sql = "SELECT IdAvion, Matricula, Modelo, Capacidad FROM Avion";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        listaAviones.Add(new Avion
+                        {
+                            IdAvion = (int)reader["IdAvion"],
+                            Matricula = reader["Matricula"].ToString(),
+                            Modelo = reader["Modelo"].ToString(),
+                            Capacidad = (int)reader["Capacidad"]
+                        });
+                    }
+                }
+            }
+            return listaAviones.AsReadOnly();
+        }
+
+        // REPOSITORIO PASAJERO
+
+
+        public void AgregarPasajero(Pasajero pasajero)
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Pasajero (Pasaporte, NombreApellido, Nacionalidad, FechaNacimiento) " +
+                             "VALUES (@Pasaporte, @NombreApellido, @Nacionalidad, @FechaNacimiento)";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Pasaporte", pasajero.Pasaporte);
+                    cmd.Parameters.AddWithValue("@NombreApellido", pasajero.NombreApellido);
+                    cmd.Parameters.AddWithValue("@Nacionalidad", pasajero.Nacionalidad);
+                    cmd.Parameters.AddWithValue("@FechaNacimiento", pasajero.FechaNacimiento);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void ModificarPasajero(Pasajero pasajero)
+        {
+            string querry = "UPDATE Pasajero SET Pasaporte=@pasaporte, NombreApellido=@nombreApellido, Nacionalidad=@nacionalidad, " +
+                "FechaNacimiento=@fechaNacimiento WHERE IdPasajero=@idPasajero";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand comando = new SqlCommand(querry, conexion);
+                comando.Parameters.AddWithValue("@pasaporte", pasajero.Pasaporte);
+                comando.Parameters.AddWithValue("@nombreApellido", pasajero.NombreApellido);
+                comando.Parameters.AddWithValue("@nacionalidad", pasajero.Nacionalidad);
+                comando.Parameters.AddWithValue("@fechaNacimiento", pasajero.FechaNacimiento);
+                comando.Parameters.AddWithValue("@idPasajero", pasajero.IdPasajero);
+
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error en la base de datos" + ex.Message);
+                }
+            }
+        }
+
+        public void EliminarPasajero(int idPasajero)
+        {
+            string querry = "DELETE From Pasaje WHERE IdPasajero=@idPasajero";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand comando = new SqlCommand(querry, conexion);
+                comando.Parameters.AddWithValue("@idPasajero", idPasajero);
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error en la base de datos" + ex.Message);
+                }
+            }
+        }
+
+        public IReadOnlyCollection<Pasajero> ListarPasajeros()
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                conn.Open();
+                string sql = "SELECT IdPasajero, Pasaporte, NombreApellido, Nacionalidad, FechaNacimiento FROM Pasajero";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        listaPasajeros.Add(new Pasajero
+                        {
+                            IdPasajero = (int)reader["IdPasajero"],
+                            Pasaporte = reader["Pasaporte"].ToString(),
+                            NombreApellido = reader["NombreApellido"].ToString(),
+                            Nacionalidad = reader["Nacionalidad"].ToString(),
+                            FechaNacimiento = (DateTime)reader["FechaNacimiento"]
+                        });
+                    }
+                }
+            }
+            return listaPasajeros.AsReadOnly();
+        }
+
+
+        // REPOSITORIO PASAJES
+
+        public void AgregarPasaje(Pasaje pasaje)
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Pasaje NumeroAsiento, FechaVuelo, IdAvion, IdPasajero) " +
+                             "VALUES (@NumeroAsiento, @FechaVuelo, @IdAvion, @IdPasajero)";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@NumeroAsiento", pasaje.NumeroAsiento);
+                    cmd.Parameters.AddWithValue("@FechaVuelo", pasaje.FechaVuelo);
+                    cmd.Parameters.AddWithValue("@IdAvion", pasaje.Avion.IdAvion);
+                    cmd.Parameters.AddWithValue("@IdPasajero", pasaje.Pasajero.IdPasajero);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void ModificarPasaje(Pasaje pasaje)
+        {
+            string querry = "UPDATE Pasaje SET NumeroAsiento=@numeroAsiento, FehaVuelo=@fechaVuelo WHERE IdPasaje=@idPasaje";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand comando = new SqlCommand(querry, conexion);
+                comando.Parameters.AddWithValue("@numeroAsiento", pasaje.NumeroAsiento);
+                comando.Parameters.AddWithValue("@fechaVuelo", pasaje.FechaVuelo);
+                comando.Parameters.AddWithValue("@idPasaje", pasaje.IdPasaje);
+
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error en la base de datos" + ex.Message);
+                }
+            }
+        }
+
+        public void EliminarPasaje(int idPasaje)
+        {
+            string querry = "DELETE From Pasaje WHERE IdPasaje=@idPasaje";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand comando = new SqlCommand(querry, conexion);
+                comando.Parameters.AddWithValue("@idPasaje", idPasaje);
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error en la base de datos" + ex.Message);
+                }
+            }
+        }
+
+        public IReadOnlyCollection<Pasaje> ListarPasajes()
+        {
+            using (SqlConnection conn = new SqlConnection(cadenaConexion))
+            {
+                conn.Open();
+                string sql = "SELECT IdPasaje, NumeroAsiento, FechaVuelo, IdAvion, IdPasajero FROM Pasaje";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    { 
+                        Pasaje pasaje = new Pasaje();
+                        Avion avion = new Avion();
+                        Pasajero pasajero = new Pasajero();
+                        pasaje.IdPasaje = reader.GetInt32(0);
+                        pasaje.NumeroAsiento = reader.GetString(1);
+                        pasaje.FechaVuelo = reader.GetDateTime(2);
+                        avion = listaAviones.FirstOrDefault(x => x.IdAvion == reader.GetInt32(3));
+                        pasajero = listaPasajeros.FirstOrDefault(x => x.IdPasajero == reader.GetInt32(4));
+                        listaPasajes.Add(pasaje);
+                    }
+                }
+            }
+            return listaPasajes.AsReadOnly();
+        }
+
+    }
+}
